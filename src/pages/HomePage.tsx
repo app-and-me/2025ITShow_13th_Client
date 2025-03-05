86a3290220b18e03a30fb612b7361f007344bb59
3x-haust <lyusungyun@icloud.com>
2025-03-05 13:33:50 +0900

    feat: 시작 타이틀 페이지

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,63 @@
+import { styled } from "styled-components";
+import backgroundImg from "/assets/imgs/background.png";
+import theme from "../styles/theme";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  background-image: url(${backgroundImg}); 
+  background-size: cover;
+  background-repeat: no-repeat;
+  
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+
+  padding-bottom: 10%;
+  padding-top: 10%;
+`;
+
+const Title = styled.img`
+  width: 60%;
+  height: auto;
+`;
+
+const Button = styled.button`
+  color: ${theme.gray.bg100};
+  text-align: center;
+
+  font-family: 'Pretendard';
+  font-size: 1.75rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 160%;
+  letter-spacing: -0.0053rem;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100%;
+  padding: 0 10%;
+
+  gap: 3.125rem;
+`
+
 export const HomePage = () => {
+  console.log(theme.gray.bg100);
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container>
+      <Title src="/assets/imgs/title.png" />
+      <ButtonContainer>
+        <Button onClick={() => navigate('/rank')}>랭킹 보기</Button>
+        <Button>클릭해서 시작하기</Button>
+      </ButtonContainer>
+    </Container>
   );
 }
